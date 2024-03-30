91bfdd7d704e8fdf6693bb1617366ac4d34c1b11
zen <zenon.deviagge@gmail.com>
2024-03-30 18:29:02 -0300

    Setup ColorList basic structure. Configure `colors` props on component imported on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
+import Values from 'values.js'
 
 const App = () => {
-  return <main>
-    <Form/>
-    <ColorList/>
-    </main>;
+  const [colors, setColors] = useState(new Values('#f15025').all(10))
+  return (
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+    </main>
+  )
 };
 export default App;
 
--- src/Form.jsx
+++ src/Form.jsx
@@ -4,17 +4,23 @@ const Form = () => {
   const [color, setColor] = useState('')
   const handleSubmit = (e) => {}
   return (
-    <section className='container'>
-      <h4>color generator</h4>  
-      <form className='color-form' onSubmit={handleSubmit}>
-        <input 
-         type="text"
-         value={color}
-         onChange={(e) => setColor(e.target.value)} 
-         placeholder='#f15025'/>
-         <button className="btn" type='submit' style={{background: color}}>
-            submit
-         </button>
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15025"
+        />
+        <button className="btn" type="submit" style={{ background: color }}>
+          submit
+        </button>
       </form>
     </section>
   )
