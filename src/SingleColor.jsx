8455adf91d315bd6d117f7c1b1dbab6ba618163b
zen <zenon.deviagge@gmail.com>
2024-03-31 23:27:21 -0300

    Build SingleColor Component with tints and shades.

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,10 @@
 const SingleColor = ({index, color}) => {
-  console.log(color);
+  const {hex, weight} = color
   return (
-    <div>SingleColor</div>
+    <article className={index > 10 ? 'color color-light' : 'color' } style={{background: `#${hex}`}}>
+     <p className="percent-value">{weight}%</p>
+     <p className="color-value">#{hex}</p>
+    </article>
   )
 }
 export default SingleColor
