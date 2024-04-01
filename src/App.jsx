680bc69a5eba9e3dcf4a12f72e00434664ebbf6c
zen <zenon.deviagge@gmail.com>
2024-03-31 23:44:29 -0300

    Import and test ReactToastify Library. Add ToastContainer component to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
-import Values from 'values.js'
+import Values from 'values.js';
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
-  const [colors, setColors] = useState(new Values('#f15025').all(10))
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
+
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer  position="top-center"/>
     </main>
   )
 };
