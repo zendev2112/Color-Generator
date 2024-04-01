b1eb961b15988cca4c42ac46cfcbc72bb003046e
zen <zenon.deviagge@gmail.com>
2024-04-01 00:00:44 -0300

    Fix preventDefault() method bug on Form component.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,18 @@ import { ToastContainer, toast } from "react-toastify";
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
 
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10)
+      setColors(newColors)
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor}/>
       <ColorList colors={colors} />
       <ToastContainer  position="top-center"/>
     </main>
